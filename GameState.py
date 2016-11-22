771aab180cf1f166f49179c7d2b61b31519e5ad7
Schoolio <zps1101@jagmail.southalabama.edu>
2016-11-22 17:58:48 -0600

    Initial Commit

--- GameState.py
+++ GameState.py
@@ -1,18 +1,18 @@
 #Zac Smith
-import Player
-
+import Pieces
+from Pieces import Pawn, King, Rook, Queen, Bishop, Knight
 
 #Gamestate handles the players and turns
 #Player has access to pieces to build the piece list
 class GameState:
     def __init__(self):
-        self.board = []
-        self.whitesTurn = False
-        self.whitePlayer = Player.Player(True)
-        self.blackPlayer = Player.Player(False)
+        self.whitesTurn = True
+        self.whitePlayer = []
+        self.blackPlayer = []
+        self.reset()
 
     #Used to reset the game state to a starting position
     def reset(self):
-        self.whitePlayer.startState()
-        self.blackPlayer.startState()
+        for x in range(0, 8):
+            self.whitePlayer.append(Pawn.Pawn((x, 6)))
         self.whitesTurn = True
--- Source.py
+++ Source.py
@@ -1,16 +1,16 @@
 __author__ = 'Zac'
 
-import Graphics, pygame, sys, Player, GameState
+import Graphics, pygame, sys, Player, GameState, Pieces
 from pygame.locals import *
+from Pieces import Pawn
 
 
 pygame.init()
 myDisplay = pygame.display.set_mode((1000, 800), 0, 32)
 Graphics.build_board(myDisplay)
-gamestate = GameState.GameState()
+gameState = GameState.GameState()
 
-
-Graphics.build_pieces(myDisplay, gamestate.whitePlayer.pieces)
+Graphics.build_pieces(myDisplay, gameState.whitePlayer)
 pygame.display.update()
 while True:
     for event in pygame.event.get():
