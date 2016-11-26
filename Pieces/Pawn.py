2cfb1e537e209bbcdc587a243d5517bed6e2f7ba
shawynkane <smk1222@jagmail.southalabama.edu>
2016-11-25 22:18:03 -0600

    Updated the all of the pieces. Deleted the "Pieces" class. Updated the TODO file.

--- Pieces/Pawn.py
+++ Pieces/Pawn.py
@@ -1,10 +1,11 @@
 __author__ = 'Zac'
 
+
 class Pawn:
     def __init__(self, location, isWhite):
         self.name = "Pawn"
-        self.canDoubleMove = True
         self.value = 1
+        self.initialMove = True
         self.location = location
         self.image = None
         self.isWhite = isWhite
@@ -13,3 +14,21 @@ class Pawn:
             self.image = "bin/Pawn_W.png"
         else:
             self.image = "bin/Pawn_B.png"
+
+    def move(self, twice=False):
+        newlocation = None
+        if (twice is True) and (self.initialMove is True):
+            newlocation = (self.location[0] + 2, self.location[1])
+        else:
+            newlocation = (self.location[0] + 1, self.location[1])
+        initialMove = False;
+        # if check(newlocation):
+        #   pass
+        # else:
+        #   return False
+        self.location[0] = newlocation[0]
+        self.locatoin[1] = newlocation[1]
+        return True
+
+    def attack(self, left):
+        pass
--- Pieces/Rook.py
+++ Pieces/Rook.py
@@ -1,5 +1,21 @@
 __author__ = 'Zac'
 
+
 class Rook:
-    def __init__(self):
+    def __init__(self, location, isWhite):
+        self.name = "Rook"
         self.value = 5
+        self.location = location
+        self.image = None
+        self.isWhite = isWhite
+
+        if self.isWhite is True:
+            self.image = "bin/Rook_W.png"
+        else:
+            self.image = "bin/Rook_B.png"
+
+    def move(self):
+        pass
+
+    def attack(self):
+        pass
