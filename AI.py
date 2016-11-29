99432f062a6e4f2761e44d830e829bfaaf485cea
shawynkane <smk1222@jagmail.southalabama.edu>
2016-11-29 12:32:00 -0600

    Updated the get_valid_moves() for the King. Made a correction in the AI.py file.

--- AI.py
+++ AI.py
@@ -4,7 +4,7 @@ import GameState
 
 
 def make_move(gameState, target):
-    validMoves = gameState.selectedPiece.get_valid_moves(gameState.selectedPiece, gameState.pieces)
+    validMoves = gameState.selectedPiece.get_valid_moves(gameState.pieces, gameState.selectedPiece)
     print("Valid moves", validMoves)
     for x in validMoves[:]:
         if x == target:
