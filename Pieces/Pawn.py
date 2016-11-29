9dad2fbc89281cf3965b7e50c20c60b1deae9cab
Schoolio <zps1101@jagmail.southalabama.edu>
2016-11-29 13:30:09 -0600

    Added to gamestate.score/status added to graphics source has just been modified for test purposes, run it if you want to see where we at atm

--- Pieces/Pawn.py
+++ Pieces/Pawn.py
@@ -2,19 +2,16 @@ __author__ = 'Zac, Shawyn Kane'
 import Pieces
 
 class Pawn:
-    def __init__(self, location, is_white):
+    def __init__(self, location, isWhite):
         self.name = "Pawn"
         self.value = 1
         self.initial_move = True
         self.location = location
         self.image = None
-        self.is_white = is_white
+        self.isWhite = isWhite
 
-        if self.is_white is True: self.image = "bin/Pawn_W.png"
+        if self.isWhite is True: self.image = "bin/Pawn_W.png"
         else: self.image = "bin/Pawn_B.png"
-
-    def get_is_white(self):
-        return self.is_white
 
     def move(self, pieces, inputLocation, twice=False):
         if inputLocation is not (self.location[0], self.location[1] - 1):
@@ -48,7 +45,7 @@ class Pawn:
 
     def get_valid_moves(self, pieces, selectedPiece):
         output = []
-        if selectedPiece.get_is_white():
+        if selectedPiece.isWhite:
             if selectedPiece.initial_move:
                 blocked = False
                 for x in pieces[:]:
